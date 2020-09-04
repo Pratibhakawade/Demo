87171cfec63c5a4b894da900e352fa1a5cc5033c
pratibhakawade <pratibhakawade12@gmail.com>
2020-09-04 18:41:11 +0530

    Unit test code add

---
+++ MathLiabrarary.Nunit.Tests/CalculatorTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathLiabrarary;
+using NUnit.Framework;
+
+
+namespace MathLiabrarary.Nunit.Tests
+{
+    [TestFixture]
+   public class CalculatorTest
+    {
+        [Test]
+        public void Add_Positive_testforNUnit()
+        {
+            int expectedResult = 20;
+            Calculator c = new Calculator();
+            int actualResult = c.Add(10, 10);
+
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+        [Test]
+        public void Add_Negative_testforNUNit()
+        {
+            int expectedResult = 10;
+            Calculator c = new Calculator();
+            int actualResult = c.Add(10, 10);
+
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+
+    }
+}
+   
---
+++ MathLiabrarary.Tests/CalculatorTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathLiabrarary;
+namespace MathLiabrarary.Tests
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void Add_Positive_test()
+        {
+            int expectedResult = 20;
+            Calculator c = new Calculator();
+            int actualResult = c.Add(10, 10);
+
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+        [TestMethod]
+        public void Add_Negative_test()
+        {
+            int expectedResult = 10;
+            Calculator c = new Calculator();
+            int actualResult = c.Add(10, 10);
+
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+
+    }
+}
